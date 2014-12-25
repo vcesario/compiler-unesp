0ff0c7009bd7746e775537910c320d909ca44bbd
victor cesario <victor.cesario@hotmail.com>
2014-12-25 15:57:47 -0200

    Revert "includes ""
    This reverts commit e151cfbcacfa32cf98004a6c4e42d31ad1140529.

--- compiler-separa-str.c
+++ compiler-separa-str.c
@@ -8,76 +8,74 @@ typedef struct{
     char mne[_size];
     char op1[_size];
     char op2[_size];
-}tipoAsm;
+}astr;
 
-tipoAsm separaStr();
+astr separaStr();
 
 int main(){
     char str_input[_size];
-    tipoAsm astr;
+    astr _astr;
     do{
-        /******************/
         printf("Comando: ");
         gets(str_input);
-        /** trocar o método acima por uma captura de arquivo **/
 
-        astr = separaStr(str_input);
+        _astr = separaStr(str_input);
 
-        printf("\nROT: \t%s", astr.rot);
-        printf("\nMNE: \t%s", astr.mne);
-        printf("\nOP1: \t%s", astr.op1);
-        printf("\nOP2: \t%s", astr.op2);
+        printf("\nROT: \t%s", _astr.rot);
+        printf("\nMNE: \t%s", _astr.mne);
+        printf("\nOP1: \t%s", _astr.op1);
+        printf("\nOP2: \t%s", _astr.op2);
 
         printf("\n\n");
-    }while(1); /**ARRUMAR CONDIÇÃO**/
+    }while(1);
 
     return 0;
 }
 
-tipoAsm separaStr(char str[_size]){
+astr separaStr(char str[_size]){
     int i,j;
-    tipoAsm astr;
+    astr _astr;
 
     i = j = 0;
 
     if (str[i] != ' ' && i<strlen(str)){
         for(j=0;str[i] != ' ';j++,i++){
-            astr.rot[j] = str[i];
+            _astr.rot[j] = str[i];
         }
-        astr.rot[j] = '\0';
+        _astr.rot[j] = '\0';
     }
     else
-        strcpy(astr.rot, " ");
+        strcpy(_astr.rot, " ");
     i++;
 
     if (str[i] != ' ' && i<strlen(str)){
         for(j=0;str[i] != ' ';j++,i++){
-            astr.mne[j] = str[i];
+            _astr.mne[j] = str[i];
         }
-        astr.mne[j] = '\0';
+        _astr.mne[j] = '\0';
     }
     else
-        strcpy(astr.mne, " ");
+        strcpy(_astr.mne, " ");
     i++;
 
     if (str[i] != ' ' && i<strlen(str)){
-        for(j=0;str[i] != ',' && str[i] && str[i] != ';';j++,i++){
-            astr.op1[j] = str[i];
+        for(j=0;str[i] != ',' && str[i];j++,i++){
+            _astr.op1[j] = str[i];
         }
-        astr.op1[j] = '\0';
+        _astr.op1[j] = '\0';
     }
     else
-        strcpy(astr.op1, " ");
+        strcpy(_astr.op1, " ");
     i++;
 
     if (str[i] != ' ' && i<strlen(str)){
-        for(j=0;str[i] && str[i] != ';';j++,i++){
-            astr.op2[j] = str[i];
+        for(j=0;str[i];j++,i++){
+            _astr.op2[j] = str[i];
         }
-        astr.op2[j] = '\0';
+        _astr.op2[j] = '\0';
     }
     else
-        strcpy(astr.op2, " ");
+        strcpy(_astr.op2, " ");
 
-    return astr;
+    return _astr;
 }
